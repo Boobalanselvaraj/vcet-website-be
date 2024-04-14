abcb0884eaa78f94c4e19185e1ba2eaf94b6b260
Gokulsarav <gokul.slj@gmail.com>
2024-04-14 18:35:53 +0530

    added a gokul DB

--- vcetwebsite/settings.py
+++ vcetwebsite/settings.py
@@ -76,14 +76,25 @@ WSGI_APPLICATION = 'vcetwebsite.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.0/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.mysql',
+#         'NAME': 'users',
+#         'USER': 'root',
+#         'PASSWORD': 'Boobalan@77',
+#         'HOST': '127.0.0.1',
+#         'PORT': '3306',
+#     }
+# }
+
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.mysql',
-        'NAME': 'users',
+        'NAME': 'mastermail',
         'USER': 'root',
-        'PASSWORD': 'Boobalan@77',
-        'HOST': '127.0.0.1',
-        'PORT': '3306',
+        'PASSWORD': 'monster',
+        'HOST': 'localhost',
+        'PORT': 3306,
     }
 }
 
